27f032ea65f8fce75a932b41ec17de59c635436e
Elizabeth Parisi <parisielizabeth@gmail.com>
2018-11-21 23:37:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,5 @@
-def display_board(board = ["X", "O", " ", "O", " ", "X", " ", " ", " "])
+def turn(board)
+  display_board(board = ["X", "O", " ", "O", " ", "X", " ", " ", " "])
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
